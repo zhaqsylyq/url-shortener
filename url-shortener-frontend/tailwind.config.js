8e5067a704b3f8cb9384a979f7bba515b30dd6f6
zhaqsylyq <zhaksylyk.chalgimbayev@nu.edu.kz>
2025-01-29 16:02:13 +0500

    initial setup of tailwind completed

---
+++ url-shortener-frontend/tailwind.config.js
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "custom-gradient": "linear-gradient(to right, #3b82f6, #9333ea)", // equivalent to from-blue-500 to-purple-600
+        "custom-gradient-2": "linear-gradient(to left, #3b82f6, #f43f5e)",
+        "card-gradient": "linear-gradient(to right, #38b2ac, #4299e1)",
+      },
+      colors: {
+        navbarColor: "#ffffff",
+        btnColor: "#3364F7",
+        linkColor: "#2a5bd7",
+      },
+      boxShadow: {
+        custom: "0 0 15px rgba(0, 0, 0, 0.3)",
+        right: "10px 0px 10px -5px rgba(0, 0, 0, 0.3)",
+      },
+      fontFamily: {
+        roboto: ["Roboto", "sans-serif"],
+        montserrat: ["Montserrat"],
+      },
+    },
+  },
+
+  variants: {
+    extend: {
+      backgroundImage: ["responsive"],
+    },
+  },
+
+  plugins: [],
+};
